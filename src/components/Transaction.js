c4b0f75ec4ae44166a6aa08613ce5b1c33d57025
Abe Rudy <aberudy@gmail.com>
2017-12-20 10:25:35 -0500

    changed transaction back to presentational

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
-class Transaction extends React.Component {
-  render() {
-    const transaction = this.props.transaction;
-    return (
-      <tr>
-        <td>{transaction.posted_at}</td>
-        <td>{transaction.description}</td>
-        <td>{transaction.category}</td>
-        <td>{transaction.amount}</td>
-      </tr>
-    );
-  }
-}
+const Transaction = props => {
+  const transaction = props.transaction;
+  return (
+    <tr>
+      <td>{transaction.posted_at}</td>
+      <td>{transaction.description}</td>
+      <td>{transaction.category}</td>
+      <td>{transaction.amount}</td>
+    </tr>
+  );
+};
 
 export default Transaction;
