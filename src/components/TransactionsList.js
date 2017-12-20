c7cfeb3dc2bcccd9796cb755595c9d9ab640bc2a
github username <github email address>
2017-12-20 10:32:01 -0500

    done

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,38 +1,82 @@
-import React from 'react'
+import React from 'react';
 
-const TransactionsList = () => {
+class TransactionsList extends React.Component {
 
-  return (
-    <table className="ui celled striped padded table">
-      <tbody>
-        <tr>
-          <th>
-            <h3 className="ui center aligned header">
-              Posted At
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Description
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Category
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Amount
-            </h3>
-          </th>
-        </tr>
+  displayTrans = () => {
+    return this.props.transactions.map( (tran) => {
+      return (
+        <div>
+          <h3>{tran}</h3>
+        </div>
+      )
+    })
+  }
 
-        {"... your code here..."}
 
-      </tbody>
-    </table>
-  )
+
+  render() {
+    console.log(this.props.transactions)
+    return (
+      <div>{this.displayTrans()}</div>
+    )
+  }
 }
 
-export default TransactionsList
+// import React from 'react'
+// import Transaction from './transaction'
+//
+// class TransactionsList extends React.Component {
+//
+//   render() {
+//     return (
+//       <div>Hey!</div>
+//     )
+//   }
+// }
+
+// const TransactionsList = () => {
+// class TransactionsList extends React.Component {
+//
+//   displayTransactions = () => {
+//     return this.props.transactions.map( (trans) => {
+//       return (
+//         <div>trans.description</div>
+//       )
+//     })
+//   }
+//
+//   render() {
+//     console.log(this.props.transactions)
+//   return (
+//     <table className="ui celled striped padded table">
+//       <tbody>
+//         <tr>
+//           <th>
+//             <h3 className="ui center aligned header">
+//               Posted At
+//             </h3>
+//           </th>
+//           <th>
+//             <h3 className="ui center aligned header">
+//               Description
+//             </h3>
+//           </th>
+//           <th>
+//             <h3 className="ui center aligned header">
+//               Category
+//             </h3>
+//           </th>
+//           <th>
+//             <h3 className="ui center aligned header">
+//               Amount
+//             </h3>
+//           </th>
+//         </tr>
+//         <Transaction />
+//       </tbody>
+//     </table>
+//   )
+// }
+// }
+
+export default TransactionsList;
