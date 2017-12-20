8778c3f9e5d5f9cd11fa3a5e3b3caeed9910f40f
Gonzodamus <Dick@TheDickWard.com>
2017-12-20 10:41:02 -0500

    working but real ugly

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,14 +1,17 @@
 import React from "react";
-import TransactionComents from "./TransactionComments";
+import TransactionComments from "./TransactionComments";
 
 const Transaction = props => {
   const { transaction } = props;
-  return transaction.id === props.selectedTransaction ? (
-    <tr onClick={props.handleClick}>
-      <td id={props.id}>{transaction.posted_at}</td>
-      <td id={props.id}>{transaction.description}</td>
-      <td id={props.id}>{transaction.category}</td>
-      <td id={props.id}>{transaction.amount}</td>
+  return transaction.id.toString() === props.selectedTransaction ? (
+    <tr>
+      <tr onClick={props.handleClick}>
+        <td id={props.id}>{transaction.posted_at}</td>
+        <td id={props.id}>{transaction.description}</td>
+        <td id={props.id}>{transaction.category}</td>
+        <td id={props.id}>{transaction.amount}</td>
+      </tr>
+      <TransactionComments />
     </tr>
   ) : (
     <tr onClick={props.handleClick}>
