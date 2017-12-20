ed27d02a44eff6830a3dc0885b1cf7a9ea2e3356
tohanian <github email address>
2017-12-20 10:31:43 -0500

    pencils down

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React from 'react';
 
-const Search = () => {
-  return (
-    <div className="ui huge fluid icon input">
-      <input
-        type="text"
-        placeholder={"Search your Recent Transactions"}
-      />
-      <i className="circular search link icon"></i>
-    </div>
-  )
+class Search extends React.Component {
+  state = {
+    query: ''
+  };
+
+  handleChange = event => {
+    this.setState({
+      query: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="ui huge fluid icon input">
+        <input
+          onChange={this.handleChange}
+          value={this.state.query}
+          type="text"
+          placeholder={'Search your Recent Transactions'}
+        />
+        <i className="circular search link icon" />
+      </div>
+    );
+  }
 }
 
-export default Search
+export default Search;
